a9acaa697cbd131bb9e5a1e63484b042729dd4ed
FSCyborg <jafernandez.tampabay@gmail.com>
2019-11-16 23:39:57 -0500

    Revert "Upgrade Travis to Xenial to support newer clang-tidy versions"
    This reverts commit 4ccccb526e2494a3bead511a724e83b264e51ad2.

--- ci/travis/before_install.sh
+++ ci/travis/before_install.sh
@@ -3,11 +3,8 @@
 set -ex
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-    export DEBIAN_FRONTEND=noninteractive
-
-    sudo add-apt-repository -y "ppa:msulikowski/valgrind" # For fixing a bug with valgrind 3.11 which does not recognize the rdrand instruction
-    sudo apt-get -yq update
-    sudo apt-get -yq install valgrind
+    # Nothing to do here
+    :
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     # Nothing to do here
     :
